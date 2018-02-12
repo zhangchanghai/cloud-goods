b1e7eb70fd5af744b658fb4fb97b89329b68e295
zhangchanghai <906393745@qq.com>
2018-02-12 16:19:32 +0800

    config自动刷新

--- src/main/java/com/boot/controller/UploadController.java
+++ src/main/java/com/boot/controller/UploadController.java
@@ -4,6 +4,7 @@ import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,9 +18,9 @@ public class UploadController {
 
 	@Autowired
 	private UploadFeign uploadFeign;
-	@GetMapping("/")
-	public Object index(ModelAndView mv){
-		mv.setViewName("index");
+	@GetMapping("page/{val}")
+	public Object index(ModelAndView mv,@PathVariable String val){
+		mv.setViewName(val);
 		return mv;
 	}
 	@PostMapping("/upload")
